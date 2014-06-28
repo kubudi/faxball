fc8d1bf0aad981cdb45272e8ffe572bed33691df
FurkanSalih <furkansalihvarol@gmail.com>
2014-06-28 14:29:12 +0300

    KeyStroke implementation

--- js/listener.js
+++ js/listener.js
@@ -1,5 +1,36 @@
-$( document ).ready(function() {
-    $('#click').on('click', function(){
-       console.log('bastin');
-    })
-});
+$(document).ready(function() {
+	var Canvas = $('#canvas');
+
+	Canvas.keydown( function(event) {
+		var pressedKey = event.which
+		//console.log(pressedKey);
+		switch(pressedKey) {
+			case KEY_W: 
+				Mover.MoveUp();
+				break;
+			case KEY_S:
+				Mover.MoveDown();
+				break;
+			case KEY_A:
+				Mover.MoveLeft();
+				break;
+			case KEY_D:
+				Mover.MoveRight();
+				break;
+			case KEY_SPACE:
+				Mover.Shoot();
+				break;
+			case KEY_ESCAPE:
+				console.log("Unfocus");
+				Canvas.blur();
+
+		}
+	});
+})
+
+ var KEY_W = 87;
+ var KEY_S = 83;
+ var KEY_A = 65;
+ var KEY_D = 68;
+ var KEY_SPACE = 32;
+ var KEY_ESCAPE = 27;
---
+++ js/mover.js
@@ -0,0 +1,26 @@
+var Mover = {
+	MoveUp : function() {
+		console.log("Moved Up");
+		//TODO
+	},
+
+	MoveDown: function() {
+		console.log("Moved Down");
+		//TODO
+	},
+
+	MoveRight: function() {
+		console.log("Moved Right");
+		//TODO
+	},
+
+	MoveLeft: function() {
+		console.log("Moved Left");
+		//TODO
+	},
+
+	Shoot: function() {
+		console.log("Shoot");
+		//TODO
+	}
+}
